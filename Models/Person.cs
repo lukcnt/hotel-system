61dc4e1c62deb735e583ed3bffedb337f5880a75
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-28 16:18:56 -0300

    Create the properties of the class

--- Models/Person.cs
+++ Models/Person.cs
@@ -7,6 +7,8 @@ namespace hotel_system.Models
 {
     public class Person
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompleteName => $"{FirstName} {LastName}".ToUpper();
     }
 }
