02e7589ea5e81fcc420991b7f7c750dd1bd07f17
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-28 16:41:36 -0300

    Create the constructors of the class

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -7,6 +7,18 @@ namespace hotel_system.Models
 {
     public class Suite
     {
+        public Suite()
+        {
+
+        }
+
+        public Suite(string suiteType, int capacity, decimal dailyPrice)
+        {
+            SuiteType = suiteType;
+            Capacity = capacity;
+            DailyPrice = dailyPrice;
+        }
+
         public string SuiteType { get; set; }
         public int Capacity { get; set; }
         public decimal DailyPrice { get; set; }
